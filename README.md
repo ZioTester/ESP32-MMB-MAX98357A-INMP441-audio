43f06c7526b9fab4bd57ac6ffb2528e77194def2
Nicola De Marco <nicola@lanciano.it>
2022-04-01 00:15:02 +0200

    simple player 3 in 1. first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# ESP32-MMB-TFT_eSPI
+
+Example projects for **ESP32-DevKitC-Multimedia-Board**  **MAX98357A audio amplifier** and **INMP441 mic** by **ZioTester Lab**.
+
+Info, guides, tutorials and documentation: [HERE](http://ziotester.github.io/hardware/mmb)
+
+![](kit.jpg)
---
+++ simple-player-3-in-1/src/main.cpp
@@ -0,0 +1,90 @@
+////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+//////
+////// Info, guides and tutorials on:
+////// https://ziotester.github.io
+//////
+////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+
+#include <SPIFFS.h>
+
+#include "Arduino.h"
+#include "Audio.h"
+#include "SD.h"
+#include "WiFi.h"
+
+Audio audio;
+SPIClass spiSD(VSPI);
+
+void setup() {
+  Serial.begin(115200);
+  audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
+  audio.setVolume(VOLUME);
+
+  /*
+
+  streaming
+
+  */
+  #ifdef STREAMING_EXAMPLE
+    String ssid = SID;
+    String password = PASSWORD;
+    Serial.print("Connecting");
+    WiFi.disconnect();
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid.c_str(), password.c_str());
+    while (WiFi.status() != WL_CONNECTED) {
+      Serial.print(".");
+      delay(500);
+    }
+    Serial.println();
+    Serial.println("Connected!");
+    Serial.print("Playing streaming from: ");
+    Serial.println(STREAMING_URL);
+    audio.connecttohost(STREAMING_URL);
+  #endif
+
+  /*
+
+  file MP3 on SPIFFS
+
+  */
+  #ifdef SPIFFS_EXAMPLE
+   if (!SPIFFS.begin()) {
+     Serial.println("File system Failed");
+     for (;;) {
+     }
+   } else {
+     Serial.println("File system Successful");
+   }
+   Serial.print("Playing mp3 form SPIFFS: ");
+   Serial.println(MP3_FILE);
+   audio.connecttoFS(SPIFFS, MP3_FILE);   
+  #endif
+
+  /*
+
+  file MP3 on SD card
+
+ */
+  #ifdef SDCARD_EXAMPLE
+    spiSD.begin(SD_CLK, SD_MISO, SD_MOSI, SD_CS);
+    if(!SD.begin(SD_CS, spiSD)){
+      Serial.println("File system Failed");
+      for (;;) {
+      }
+    } else {
+      Serial.println("File system Successful");
+    }
+    Serial.print("Playing mp3 form SD Card: ");
+    Serial.println(MP3_FILE);
+    audio.connecttoFS(SD, MP3_FILE);
+  #endif
+  
+
+  // audio.connecttospeech("Hello my freind!", "en");
+}
+void loop() { audio.loop(); }
